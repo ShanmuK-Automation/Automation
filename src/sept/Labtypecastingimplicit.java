a4da4e8418e3d9445f102377e4686e8c577e7eb9
ShanmuK-Automation <shanmuk8979@gmail.com>
2024-09-19 20:19:53 +0530

    Type Casting Program

---
+++ src/sept/Labtypecastingimplicit.java
@@ -0,0 +1,10 @@
+package sept;
+
+public class Labtypecastingimplicit {
+    public static void main(String[] args) {
+        byte b = 100;
+        int i = b;//implicit
+
+        System.out.println(i);
+    }
+}
